a415e073438e916665cb362a0d50d60b2d0d3f82
Conrad Irwin <conrad.irwin@gmail.com>
2010-07-31 18:40:47 -0700

    Add new files

---
+++ lib/robotstxt/common.rb
@@ -0,0 +1,26 @@
+require 'uri'
+require 'iconv'
+require 'net/http'
+
+module Robotstxt
+  module CommonMethods
+
+    protected
+    # Convert a URI or a String into a URI
+    def objectify_uri(uri)
+
+        if uri.is_a? String
+          # URI.parse will explode when given a character that it thinks
+          # shouldn't appear in uris. We thus escape them before passing the
+          # string into the function. Unfortunately URI.escape does not respect
+          # all characters that have meaning in HTTP (esp. #), so we are forced
+          # to state exactly which characters we would like to escape.
+          uri = URI.escape(uri, %r{[^!$#%&'()*+,\-./0-9:;=?@A-Z\[\\\]_a-z~]})
+          uri = URI.parse(uri)
+        else
+          uri
+        end
+
+    end
+  end
+end
---
+++ lib/robotstxt/getter.rb
@@ -0,0 +1,81 @@
+module Robotstxt
+  class Getter
+    include CommonMethods
+
+    # Get the text of a robots.txt file from the given source, see #get.
+    def obtain(source, robot_id, options)
+      options = {
+        :num_redirects => 5,
+        :http_timeout => 10,
+        :url_charset => "utf8"
+      }.merge(options)
+
+      robotstxt = if source.is_a? Net::HTTP
+        obtain_via_http(source, "/robots.txt", robot_id, options)
+      else
+        uri = objectify_uri(source)
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.read_timeout = options[:http_timeout]
+        if uri.scheme == 'https'
+          http.use_ssl = true
+          http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        end
+        obtain_via_http(http, "/robots.txt", robot_id, options)
+      end
+    end
+
+    protected
+
+    # Recursively try to obtain robots.txt following redirects and handling the
+    # various HTTP response codes as indicated on robotstxt.org
+    def obtain_via_http(http, uri, robot_id, options)
+      response = http.get(uri, {'User-Agent' => robot_id})
+
+      begin
+        case response
+        when Net::HTTPSuccess
+          decode_body(response, options[:url_charset])
+        when Net::HTTPRedirection
+          if options[:num_redirects] > 0 && response['location']
+            options[:num_redirects] -= 1
+            obtain(response['location'], robot_id, options)
+          else
+            all_allowed
+          end
+        when Net::HTTPUnauthorized
+          all_forbidden
+        when Net::HTTPForbidden
+          all_forbidden
+        else
+          all_allowed
+        end
+      rescue Timeout::Error #, StandardError
+        all_allowed
+      end
+
+    end
+
+    # A robots.txt body that forbids access to everywhere
+    def all_forbidden
+      "User-agent: *\nDisallow: /\n"
+    end
+
+    # A robots.txt body that allows access to everywhere
+    def all_allowed
+      "User-agent: *\nDisallow:\n"
+    end
+
+    # Decode the response's body according to the character encoding in the HTTP
+    # headers.
+    # In the case that we can't decode, Ruby's laissez faire attitude to encoding
+    # should mean that we have a reasonable chance of working anyway.
+    def decode_body(response, charset)
+      Iconv.conv(charset, (response.type_params['charset'] || "ISO-8859-1"), response.body)
+    rescue Iconv::IllegalSequence, Iconv::InvalidCharacter, Iconv::InvalidEncoding
+      response.body
+    end
+
+
+  end
+
+end
